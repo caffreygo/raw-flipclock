b20315fa6c0d229c30c0dfcd024d773f7ee0c735
Jinrui <jerry.chenj@outlook.com>
2024-03-28 22:45:21 +0800

    add animation class & animationend event

---
+++ js/index.js
@@ -0,0 +1,83 @@
+class FlipClock {
+  main;
+  nums;
+  nextNums;
+  refs;
+  constructor(el) {
+    this.main = document.querySelector(el);
+  }
+
+  clock() {
+    this.getTimes();
+    this.creteSectionElement();
+    this.getRefs();
+  }
+
+  render() {
+    this.clock();
+    setInterval(() => {
+      this.getTimes();
+      this.update();
+    }, 50);
+  }
+
+  update() {
+    this.nums.forEach((num, index) => {
+      const nextNum = this.nextNums[index];
+      const [div1, div2] = this.refs[index];
+
+      if (Number(div1.dataset.before) !== num) {
+        div2.classList.add("flipDown");
+      }
+
+      div2.addEventListener("animationend", () => {
+        div1.dataset.before = num;
+        div2.dataset.before = num;
+        div1.dataset.after = nextNum;
+        div2.dataset.after = nextNum;
+        div2.classList.remove("flipDown");
+      });
+    });
+  }
+
+  getTimes() {
+    const timeStamp = Date.now();
+    this.nums = new Date(timeStamp)
+      .toLocaleTimeString()
+      .replaceAll(":", "")
+      .split("")
+      .map((num) => Number(num));
+    this.nextNums = new Date(timeStamp + 1000)
+      .toLocaleTimeString()
+      .replaceAll(":", "")
+      .split("")
+      .map((num) => Number(num));
+  }
+
+  getRefs() {
+    this.refs = Array.from(this.main.querySelectorAll("section")).map(
+      (section) => section.querySelectorAll("div")
+    );
+  }
+
+  creteSectionElement() {
+    this.nums.forEach((num, idx) => {
+      this.main.insertAdjacentHTML(
+        "beforeend",
+        `
+      <section>
+        <div data-before="${num}" data-after="${num + 1}"></div>
+        <div data-before="${num}" data-after="${num + 1}"></div>
+      </section>
+      `
+      );
+
+      if (idx % 2 & (idx < this.nums.length - 1)) {
+        this.main.insertAdjacentHTML("beforeend", "<p></p>");
+      }
+    });
+  }
+}
+
+const instance = new FlipClock("#clock");
+instance.render();
